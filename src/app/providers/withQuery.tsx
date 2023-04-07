9e965777873c6939f29ca1beebde10949704b7d3
Datmon <danilanpilov@mail.ru>
2023-04-07 22:46:22 +0600

    fix: говорит, что скоро будет деприкейтед, так что выпилил, хз что делала

--- src/app/providers/withQuery.tsx
+++ src/app/providers/withQuery.tsx
@@ -4,10 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 const withQuery = (component: () => React.ReactNode) => () =>
-  (
-    <QueryClientProvider client={queryClient} contextSharing>
-      {component()}
-    </QueryClientProvider>
-  );
+  <QueryClientProvider client={queryClient}>{component()}</QueryClientProvider>;
 
 export default withQuery;
