e1c668ee09fd1daa43e13ba61043aafc55a40fe2
Roman Gladyshev <romangladyshev@MacBook-Air-Roman.local>
2023-04-11 18:24:24 +0400

    fix(providers): import spacing

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,7 +1,6 @@
 import compose from 'compose-function';
 
-import withGesture from 'src/app/providers/withGestures';
-
+import withGesture from './withGestures';
 import withNavigation from './withNavigation';
 import withQuery from './withQuery';
 import withUI from './withUI';
